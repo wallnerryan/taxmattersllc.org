e9c6203147a6b40e00d70529eb3f99a57476cf74
Ryan Wallner <wallnerryan@gmail..com>
2015-10-13 17:36:14 -0400

    fix bug, when both MySQL and Rails are started via Compose, compose doesnt give enough time for MySQL to initialize so rake db doesnt connect, we add a sleep 15 here to make sure this is good to go
    Signed-off-by: Ryan Wallner <wallnerryan@gmail..com>

--- init.sh
+++ init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# This is to give enough time for MySQL to initialize from compose file.
+sleep 15
+
+# Create the DB and Start the app
 cd /taxmatters-app
 rake db:create
 rails server -b 0.0.0.0 
