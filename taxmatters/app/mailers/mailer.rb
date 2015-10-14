4bc975c4d07ef38dd386de96f3496b19cff70e8e
Ryan Wallner <wallnerryan@gmail..com>
2015-10-14 12:24:20 -0400

    syntax edit, clean up how mail is sent
    Signed-off-by: Ryan Wallner <wallnerryan@gmail..com>

--- taxmatters/app/mailers/mailer.rb
+++ taxmatters/app/mailers/mailer.rb
@@ -1,12 +1,12 @@
 class Mailer < ActionMailer::Base
   
   #All must be verified addresses on Amazon SES
-  default :from => "info@taxmattersllc.org"
+  default :from => "\"TaxMatters LLC\" <info@taxmattersllc.org>"
   default :to => "taxmattersllc@msn.com"
 
   def new_message(message)
     @message = message
-    mail(:subject => "Tax Matters Inquiry, #{message.subject}", :reply_to => @message.email, 
+    mail(:subject => "TaxMatters Inquiry, #{message.subject}", :reply_to => @message.email, 
          :body => "This message is from:*  #{message.email}\n\n*Senders Name:* #{message.name}\n\n\n*Message:*\n\n#{message.body}\n\n\nClick REPLY to respond")
   end
   
