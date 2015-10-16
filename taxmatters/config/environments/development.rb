cbd0ac5d8a6c0c8e44fbec978d8c51cf48d7154b
Ryan Wallner <wallnerryan@gmail..com>
2015-10-16 11:15:36 -0400

    clean development.rb
    Signed-off-by: Ryan Wallner <wallnerryan@gmail..com>

--- taxmatters/config/environments/development.rb
+++ taxmatters/config/environments/development.rb
@@ -54,8 +54,8 @@ Taxmatters::Application.configure do
  config.action_mailer.delivery_method = :smtp
  config.action_mailer.smtp_settings = {
      :address => "email-smtp.us-east-1.amazonaws.com",
-      :user_name => "AKIAIWW25PYGKM7XJ2BQ",
-      :password => "Ap1uV4tymCB5lZGcDZE6QNhfKyS96Zim3WjObWxzY4S/",
+      :user_name => "XX",
+      :password => "XXXX",
       :authentication => :login,
       :enable_starttls_auto => true
  }
